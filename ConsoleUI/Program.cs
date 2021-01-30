bdc2ec860dba80cd90d82681bc2765164bd004f2
şükrü akkoyun <Sukruakkoyun87@hotmail.com>
2021-01-31 00:10:02 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http.Headers;
+using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new InMemoryProductDal());
+
+            foreach (var product in productManager.GetAll())
+            {
+                Console.WriteLine("Ürün Adi : {0}  ",product.ProductName);
+            }
+            
+        }
+    }
+}
